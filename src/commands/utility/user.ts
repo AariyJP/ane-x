aa9749ed0cf850bd4249f45b8b618fa977314451
AariyJP <aariyjp@aariy.net>
2025-07-30 16:02:01 +0900

    Refactor:

--- src/commands/utility/user.ts
+++ src/commands/utility/user.ts
@@ -1,14 +1,16 @@
-import { BaseInteraction, CommandInteraction, Interaction } from "discord.js";
+import { BaseInteraction, CommandInteraction, Interaction } from "discord.js"
 
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js")
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Provides information about the user.'),
-	async execute(interaction: CommandInteraction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member}.`);
-	},
-};
+    data: new SlashCommandBuilder()
+        .setName("user")
+        .setDescription("Provides information about the user."),
+    async execute(interaction: CommandInteraction) {
+        // interaction.user is the object representing the User who ran the command
+        // interaction.member is the GuildMember object, which represents the user in the specific guild
+        await interaction.reply(
+            `This command was run by ${interaction.user.username}, who joined on ${interaction.member}.`,
+        )
+    },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,64 @@
-import { ApplicationCommandOptionType, REST, Routes, Client, Events, GatewayIntentBits, Partials, SnowflakeUtil, SlashCommandBuilder } from 'discord.js';
-import dotenv from 'dotenv';
+import {
+    REST,
+    Routes,
+    Client,
+    Events,
+    GatewayIntentBits,
+    Partials,
+    SlashCommandBuilder,
+} from "discord.js";
+import { TwitterApi } from "twitter-api-v2";
+import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
-const token = process.env.DISCORD_BOT_TOKEN || '';
+const token = process.env.DISCORD_BOT_TOKEN || "";
+const xtoken = process.env.X_BEARER_TOKEN || "";
+const clientId = process.env.X_CLIENT_ID || "";
+const clientSecret = process.env.X_CLIENT_SECRET || "";
+const appKey = process.env.X_APP_KEY || "";
+const appSecret = process.env.X_APP_SECRET || "";
+const accessToken = process.env.X_ACCESS_TOKEN || "";
+const accessSecret = process.env.X_ACCESS_SECRET || "";
 
+process.on('uncaughtException', (err) => {
+  console.error(err);
+});
 
-const commands = [new SlashCommandBuilder()
-.setName('x')
-.setDescription('x')
-.addStringOption(option =>
-    option
-        .setName('status')
-        .setDescription('status')
-        .setRequired(true)
-)
-.toJSON()];
+// const XApp = new TwitterApi({ clientId: clientId, clientSecret: clientSecret });
+const XApp = new TwitterApi({
+    appKey: appKey,
+    appSecret: appSecret,
+    accessToken: accessToken,
+    accessSecret: accessSecret,
+});
 
-const rest = new REST({ version: '10' }).setToken(token);
-
-
+const commands = [
+    new SlashCommandBuilder()
+        .setName("x")
+        .setDescription("x")
+        .addStringOption((option) =>
+            option.setName("status").setDescription("status").setRequired(true)
+        )
+        .addAttachmentOption((option) =>
+            option.setName("image0").setDescription("image0").setRequired(false)
+        )
+        .addIntegerOption((option) =>
+            option.setName("user").setDescription("user").setRequired(false)
+        )
+        .setDefaultMemberPermissions(0)
+        .toJSON(),
+    new SlashCommandBuilder()
+        .setName("xoauth2")
+        .setDescription("xoauth2")
+        .setDefaultMemberPermissions(0)
+        .toJSON(),
+    new SlashCommandBuilder()
+        .setName("xverify")
+        .setDescription("xverify")
+        .setDefaultMemberPermissions(0)
+        .toJSON(),
+];
+const rest = new REST({ version: "10" }).setToken(token);
 
 // Define the intents your bot needs
 const client = new Client({
@@ -33,25 +72,35 @@ const client = new Client({
 });
 
 client.once(Events.ClientReady, async () => {
-
     console.log(`${client.user?.tag} でログインしました。`);
     try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(Routes.applicationCommands(client.user?.id || ''), { body: commands });
-
-        console.log('Successfully reloaded application (/) commands.');
+        await rest.put(Routes.applicationCommands(client.user?.id || ""), {
+            body: commands,
+        });
+        console.log("アプリケーションコマンドを登録しました。");
     } catch (error) {
         console.error(error);
     }
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'x') {
-    await interaction.reply('Pong!');
-  }
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    switch (interaction.commandName) {
+        case "xoauth2":
+            // await interaction.reply(XApp.generateOAuth2AuthLink("http://localhost", {scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access']}).url)
+            break;
+        case "xverify":
+            // await interaction.reply("xverify command executed.")
+            break;
+        case "x":
+            await interaction.deferReply({ flags: "Ephemeral" });
+            const status = interaction.options.getString("status")!;
+            await interaction.editReply({content: status});
+            break;
+        default:
+            await interaction.reply("Unknown command.");
+            break;
+    }
 });
 
 client.login(token);
